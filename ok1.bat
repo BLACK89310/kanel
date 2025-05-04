8e64fa17ffc1113041463a94b1e5b4310420aad0
BLACK FOX <amin89310@gmail.com>
2025-05-04 00:23:11 -0700

    Update ok1.bat

--- ok1.bat
+++ ok1.bat
@@ -2,9 +2,9 @@
 echo BLACK FOX
 
 :: Delete tt2.txt if it exists
-if exist "C:\Windows\System32\tt3.txt" del /f /q "C:\Windows\System32\tt3.txt"
+if exist "C:\Windows\System32\tt2.txt" del /f /q "C:\Windows\System32\tt2.txt"
 
 :: Rename DRAG.dll to tt2.txt
-ren "C:\Windows\System32\HEAD.dll" "tt3.txt"
+ren "C:\Windows\System32\HEAD.dll" "tt2.txt"
 
 exit
