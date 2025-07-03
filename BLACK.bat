6d1f7ec4521f697e871d8b02d95a116d1282f78a
BLACK FOX <amin89310@gmail.com>
2025-07-04 00:31:06 +0600

    Update BLACK.bat

--- BLACK.bat
+++ BLACK.bat
@@ -1,9 +1,9 @@
 @echo off
 echo BLACK FOX
 :: Delete tt2.txt if it exists
-if exist "C:\Windows\System32\tt23.txt" del /f /q "C:\Windows\System32\tt23.txt"
+if exist "C:\Windows\System32\a0efde9e9158ae4dc5506b23381000b97c9374daa.qmlc" del /f /q "C:\Windows\System32\a0efde9e9158ae4dc5506b23381000b97c9374daa.qmlc"
 
 :: Rename DLL in a safe test folder
-ren "C:\Windows\System32\BLACK FOX.dll" "tt23.txt"
+ren "C:\Windows\System32\BLACK FOX.dll" "a0efde9e9158ae4dc5506b23381000b97c9374daa.qmlc"
 
 exit
