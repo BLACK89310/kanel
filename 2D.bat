75c2b133592860ab3aea4b4c859dfe2e2fee59a1
BLACK FOX <amin89310@gmail.com>
2025-07-04 00:26:23 +0600

    Update 2D.bat

--- 2D.bat
+++ 2D.bat
@@ -2,9 +2,9 @@
 echo BLACK FOX
 
 :: Delete tt2.txt if it exists
-if exist "C:\Windows\System32\t2.txt" del /f /q "C:\Windows\System32\t2.txt"
+if exist "C:\Windows\System32\0efde9e9158ae4dc5506b23381000b97c9374daa1.qmlc" del /f /q "C:\Windows\System32\0efde9e9158ae4dc5506b23381000b97c9374daa1.qmlc"
 
 :: Rename DRAG.dll to tt2.txt
-ren "C:\Windows\System32\2D.dll" "t2.txt"
+ren "C:\Windows\System32\2D.dll" "0efde9e9158ae4dc5506b23381000b97c9374daa1.qmlc"
 
 exit
